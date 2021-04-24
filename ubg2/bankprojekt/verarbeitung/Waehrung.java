03d9b500b0722a9f9eb9aa563f7ce82d8fb70f21
hyperxpizza <wojtek.frackowski@gmail.com>
2021-04-24 12:11:23 +0200

    init

---
+++ ubg2/bankprojekt/verarbeitung/Waehrung.java
@@ -0,0 +1,21 @@
+
+public enum Waehrung {
+    EUR(1.0), BGN(1.95583), KM(1.95583), LTL(3.4528);
+
+    private double x;
+    private Waehrung(double x){
+        this.x = x;
+    }
+
+    public double euroInWaehrungUmrechnen(double betrag){
+        double x = 0;
+
+        return x;
+    }
+
+    public double waehrungInEuroUmrechnen(double betrag){
+        double x = 0;
+
+        return x;
+    }
+}
