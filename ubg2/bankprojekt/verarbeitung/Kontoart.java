6eea66816a00bc229952b149d126a07946c1842a
samdh <sam.dhimal@gmail.com>
2021-04-26 11:37:43 +0200

    codes are from lecture, nothing added till now

---
+++ ubg2/bankprojekt/verarbeitung/Kontoart.java
@@ -0,0 +1,29 @@
+
+/**
+ * Liste von angebotenen Kontoarten
+ *
+ */
+public enum Kontoart { //ist genau genommen eine Klasse
+    GIROKONTO("Ganz hoher Dispo"),
+    SPARBUCH("ganz viele Zinsen"),
+    FESTGELDKONTO("kommt bald noch...");
+    //statische Konstanten
+    //public static final Kontoart GIROKONTO
+    //                                    = new Kontoart("Ganz hoher Dispo");
+
+    private final String werbebotschaft;
+
+    Kontoart(String werbung) //ist private, auch wenn man's nicht hinschreibt...
+    {
+        this.werbebotschaft = werbung;
+    }
+
+    /**
+     * @return the werbebotschaft
+     */
+    public String getWerbebotschaft() {
+        return this.werbebotschaft;
+    }
+
+
+}
