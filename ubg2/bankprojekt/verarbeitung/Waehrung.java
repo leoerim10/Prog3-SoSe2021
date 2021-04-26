14aa0ee6c4565175bd9619f5a670d1a305a80a02
samdh <sam.dhimal@gmail.com>
2021-04-26 13:45:57 +0200

    enum with comments added

---
+++ ubg2/bankprojekt/verarbeitung/Waehrung.java
@@ -0,0 +1,30 @@
+/**
+ *ordnet die Wehrungen zu und rechnet sie um
+ * @author Sameer Dhimal 569076,  Wojciech Maximilan Frackowski 576278
+ */
+public enum Waehrung {
+    EUR(1.0), BGN(1.95583), KM(1.95583), LTL(3.4528);
+
+    private double x;
+    Waehrung(double x){
+        this.x = x;
+    }
+
+    /**
+     *rechnet in Euro angegebenen Betrag in die jeweilige Währung um
+     * @param betrag betrag umzurechnen
+     * @return umgerechnete Betrag
+     */
+    public double euroInWaehrungUmrechnen(double betrag){
+        return betrag * this.x;
+    }
+
+    /**
+     *rechnet in this-Währung angegebenen Betrag in Euro um
+     * @param betrag gegebene Betrag umzurechnen
+     * @return umgerechnete Betrag
+     */
+    public double waehrungInEuroUmrechnen(double betrag){
+        return betrag / this.x;
+    }
+}
