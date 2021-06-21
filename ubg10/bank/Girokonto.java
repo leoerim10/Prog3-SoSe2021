2cb493c64508e1eca92bc55da044697eda5f5385
hyperxpizza <wojtek.frackowski@gmail.com>
2021-06-21 20:10:54 +0200

    task 1

--- ubg10/bank/Girokonto.java
+++ ubg10/bank/Girokonto.java
@@ -5,64 +5,66 @@ import java.io.Serializable;
  * @author Doro
  *
  */
-public class Girokonto extends Konto implements Ueberweisungsfaehig, Serializable{
-	/**
-	 * Wert, bis zu dem das Konto berzogen werden darf
-	 */
-	private double dispo;
+public class Girokonto extends Konto implements Ueberweisungsfaehig, Serializable {
+    /**
+     * Wert, bis zu dem das Konto überzogen werden darf
+     */
+    private double dispo;
 
-	/**
-	 * erzeugt ein leeres, nicht gesperrtes Standard-Girokonto
-	 * von Herrn MUSTERMANN
-	 */
-	public Girokonto()
-	{
-		super(Kunde.MUSTERMANN, 99887766);
-		this.dispo = 500;
-	}
-	
-	/**
-	 * erzeugt ein Girokonto mit den angegebenen Werten
-	 * @param inhaber Kontoinhaber
-	 * @param nummer Kontonummer
-	 * @param dispo Dispo
-	 * @throws IllegalArgumentException wenn der inhaber null ist oder der angegebene dispo negativ bzw. NaN ist
-	 */
-	public Girokonto(Kunde inhaber, long nummer, double dispo)
-	{
-		//nur Kommentare...
-		super(inhaber, nummer);
-		if(dispo < 0 || Double.isNaN(dispo))
-			throw new IllegalArgumentException("Der Dispo ist nicht gultig!");
-		this.dispo = dispo;
-	}
-	
-	/**
-	 * liefert den Dispo
-	 * @return Dispo von this
-	 */
-	public double getDispo() {
-		return dispo;
-	}
-
-	/**
-	 * setzt den Dispo neu
-	 * @param dispo muss grosser sein als 0
-	 * @throws IllegalArgumentException wenn dispo negativ bzw. NaN ist
-	 */
-	public void setDispo(double dispo) {
-		if(dispo < 0 || Double.isNaN(dispo))
-			throw new IllegalArgumentException("Der Dispo ist nicht gültig!");
-		this.dispo = dispo;
-	}
-	
-	@Override
-    public boolean ueberweisungAbsenden(double betrag, 
-    		String empfaenger, long nachKontonr, 
-    		long nachBlz, String verwendungszweck) 
-    				throws GesperrtException 
+    /**
+     * erzeugt ein leeres, nicht gesperrtes Standard-Girokonto
+     * von Herrn MUSTERMANN
+     */
+    public Girokonto()
     {
-      if (this.isGesperrt())
+        super(Kunde.MUSTERMANN, 99887766);
+        this.dispo = 500;
+    }
+
+
+
+    /**
+     * erzeugt ein Girokonto mit den angegebenen Werten
+     * @param inhaber Kontoinhaber
+     * @param nummer Kontonummer
+     * @param dispo Dispo
+     * @throws IllegalArgumentException wenn der inhaber null ist oder der angegebene dispo negativ bzw. NaN ist
+     */
+    public Girokonto(Kunde inhaber, long nummer, double dispo)
+    {
+        //nur Kommentare...
+        super(inhaber, nummer);
+        if(dispo < 0 || Double.isNaN(dispo))
+            throw new IllegalArgumentException("Der Dispo ist nicht gültig!");
+        this.dispo = dispo;
+    }
+
+    /**
+     * liefert den Dispo
+     * @return Dispo von this
+     */
+    public double getDispo() {
+        return dispo;
+    }
+
+    /**
+     * setzt den Dispo neu
+     * @param dispo muss größer sein als 0
+     * @throws IllegalArgumentException wenn dispo negativ bzw. NaN ist
+     */
+    public void setDispo(double dispo) {
+        if(dispo < 0 || Double.isNaN(dispo))
+            throw new IllegalArgumentException("Der Dispo ist nicht gÃ¼ltig!");
+        this.dispo = dispo;
+    }
+
+    @Override
+    public boolean ueberweisungAbsenden(double betrag,
+                                        String empfaenger, long nachKontonr,
+                                        long nachBlz, String verwendungszweck)
+            throws GesperrtException
+    {
+        if (this.isGesperrt())
             throw new GesperrtException(this.getKontonummer());
         if (betrag < 0 || Double.isNaN(betrag) || empfaenger == null || verwendungszweck == null)
             throw new IllegalArgumentException("Parameter fehlerhaft");
@@ -72,7 +74,7 @@ public class Girokonto extends Konto implements Ueberweisungsfaehig, Serializabl
             return true;
         }
         else
-        	return false;
+            return false;
     }
 
     @Override
@@ -82,30 +84,43 @@ public class Girokonto extends Konto implements Ueberweisungsfaehig, Serializabl
             throw new IllegalArgumentException("Parameter fehlerhaft");
         setKontostand(getKontostand() + betrag);
     }
-    
+
     @Override
     public String toString()
     {
-    	String ausgabe = "-- GIROKONTO --" + System.lineSeparator() +
-    	super.toString()
-    	+ "Dispo: " + this.dispo + System.lineSeparator();
-    	return ausgabe;
+        String ausgabe = "-- GIROKONTO --" + System.lineSeparator() +
+                super.toString()
+                + "Dispo: " + this.dispo + System.lineSeparator();
+        return ausgabe;
     }
 
-	@Override
-	public boolean abheben(double betrag) throws GesperrtException{
-		if (betrag < 0 || Double.isNaN(betrag)) {
-			throw new IllegalArgumentException("Betrag ungültig");
-		}
-		if(this.isGesperrt())
-			throw new GesperrtException(this.getKontonummer());
-		if (getKontostand() - betrag >= - dispo)
-		{
-			setKontostand(getKontostand() - betrag);
-			return true;
-		}
-		else
-			return false;
-	}
+
+    @Override
+    public boolean abhebenGiro(double betrag) throws GesperrtException{
+        if (betrag < 0 || Double.isNaN(betrag)) {
+            throw new IllegalArgumentException("Betrag ungueltig");
+        }
+        if(this.isGesperrt())
+            throw new GesperrtException(this.getKontonummer());
+        if (getKontostand() - betrag >= - dispo)
+        {
+            setKontostand(getKontostand() - betrag);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    @Override
+    public boolean abhebenSpar(double betrag) {
+        return false;
+    }
+
+
+    @Override
+    public void waehrungswechsel(Waehrung neu) {
+        dispo = Waehrung.waehrungZuWaehrung(dispo, this.getAktuelleWaehrung(), neu);
+        super.waehrungswechsel(neu);
+    }
 
 }
