cc90da6c0935737dc7dd22cd524db231b0248004
hyperxpizza <wojtek.frackowski@gmail.com>
2021-06-28 17:09:21 +0200

    add new observer methods

--- ubg11/bank/KontoObserver.java
+++ ubg11/bank/KontoObserver.java
@@ -14,7 +14,22 @@ public class KontoObserver implements PropertyChangeListener{
         if(e.getPropertyName().equals("Inhaber")) {
         	Kunde oldInhaber = (Kunde) e.getOldValue();
         	Kunde newInhaber = (Kunde) e.getNewValue();
-        	System.out.println("Konto owner changed from: " + oldInhaber.getName() + " to: " + newInhaber.getName());
+        	System.out.println("[*] Konto owner changed from: " + oldInhaber.getName() + " to: " + newInhaber.getName());
+        }
+        if(e.getPropertyName().equals("Sperren")) {
+        	Konto k = (Konto) e.getSource();
+        	String status = "";
+        	if (k.getGesperrtText() == "GESPERRT") {
+        		status = "GESPERRT";
+        	} else {
+        		status = "NOT GESPERRT";
+        	}
+        	System.out.println("[*] Konto: " + k.getKontonummer() + "is " + status);
+        }
+        if(e.getPropertyName().equals("Waehrung")) {
+        	Waehrung oldWaehrung = (Waehrung) e.getOldValue();
+        	Waehrung newWaehrung = (Waehrung) e.getNewValue();
+        	System.out.println("[*] Old Waehrung: " + oldWaehrung.name() + " New Waehrung: " + newWaehrung.name());
         }
     }
 }
